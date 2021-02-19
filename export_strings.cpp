dafb516ffbca043600bdc6d3aeb689e6c49b8986
yanke928 <yanke0928@gmail.com>
2021-02-19 22:11:50 +0800

    Font number limitation

--- export_strings.cpp
+++ export_strings.cpp
@@ -12,12 +12,9 @@
  * bit 7: Render option: erase
  */
 
-static uint8_t create_header(uint8_t fontId, bool isNonLanguaged, UIString::AlignType isMiddle, bool isErase)
+static uint8_t create_header(bool isNonLanguaged, UIString::AlignType isMiddle, bool isErase)
 {
-    if (fontId > 0xF) {
-        throw std::runtime_error("Too much font, maximum 15");
-    }
-    return (fontId & 0xF) | (isNonLanguaged ? (1 << 4) : 0) | (((uint8_t)isMiddle) << 5U) | (isErase ? (1 << 7) : 0);
+    return (isNonLanguaged ? (1 << 4) : 0) | (((uint8_t)isMiddle) << 5U) | (isErase ? (1 << 7) : 0);
 }
 
 void export_strings(std::vector<UIString> &src, std::ostream & oss)
@@ -27,9 +24,14 @@ void export_strings(std::vector<UIString> &src, std::ostream & oss)
     for (auto &str : src)
     {
         str.Pos = buf.size();
+
         if (str.Default) // non-languaged
         {
-            buf.push_back(create_header(str.Default->Font->Id, true, str.Align, str.Erase));
+            if (str.Default->Font->Id > 0xFF) {
+                throw std::runtime_error("Too much font, maximum 255");
+            }
+            buf.push_back(create_header(true, str.Align, str.Erase));
+            buf.push_back(str.Default->Font->Id);
             for (char ch : str.Default->Value)
             {
                 buf.push_back((uint8_t) ch);
@@ -40,7 +42,11 @@ void export_strings(std::vector<UIString> &src, std::ostream & oss)
         {
             for (const auto &slang : str.Langs)
             {
-                buf.push_back(create_header(slang.second.Font->Id, false, str.Align, str.Erase));
+                buf.push_back(create_header(false, str.Align, str.Erase));
+                if (slang.second.Font->Id > 0xFF) {
+                    throw std::runtime_error("Too much font, maximum 255");
+                }
+                buf.push_back(slang.second.Font->Id);
                 for (char ch : slang.second.Value)
                 {
                     buf.push_back((uint8_t) ch);
